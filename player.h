6b9867437a95c0941edc43a829be556a11a21838
Almyra Ramadhina <almyrmdhn@gmail.com>
2018-11-29 13:55:07 +0700

    Create player.h

---
+++ player.h
@@ -0,0 +1,12 @@
+#ifndef player_H
+#define player_H
+
+typedef struct {
+    int Life;
+    int Money;
+    int Time;       // prog utama
+    char Name[11];
+    int RuangPlayer;
+} Pemain;
+
+#endif
